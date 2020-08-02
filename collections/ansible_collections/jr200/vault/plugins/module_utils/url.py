0606d8bf02527b08bc6057d238c2f4aa724af5c6
Jayshan Raghunandan <jayshan.raghunandan@bhdgsystematic.com>
2020-08-02 16:11:22 +0100

    better name for variable

--- collections/ansible_collections/jr200/vault/plugins/module_utils/url.py
+++ collections/ansible_collections/jr200/vault/plugins/module_utils/url.py
@@ -3,13 +3,13 @@ from ansible.module_utils.urls import open_url
 from json import loads
 
 
-def get(path, token, vault_addr, vault_cacert, response_name):
+def get(url_path, token, vault_addr, vault_cacert, response_name):
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     if token:
         headers['X-Vault-Token'] = token
 
     try:
-        url = '/'.join([vault_addr, 'v1', path])
+        url = '/'.join([vault_addr, 'v1', url_path])
         response = open_url(url, None, headers, 'GET', ca_path=vault_cacert)
         return {response_name: loads(response.read().decode("utf-8"))}
 
@@ -17,13 +17,13 @@ def get(path, token, vault_addr, vault_cacert, response_name):
         return {"errors": to_text(e)}
 
 
-def post(path, token, vault_addr, vault_cacert, payload=None):
+def post(url_path, token, vault_addr, vault_cacert, payload=None):
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     if token:
         headers['X-Vault-Token'] = token
 
     try:
-        url = '/'.join([vault_addr, path])
+        url = '/'.join([vault_addr, url_path])
         response = open_url(url, payload, headers, 'POST',
                             ca_path=vault_cacert)
         return loads(response.read().decode("utf-8"))
